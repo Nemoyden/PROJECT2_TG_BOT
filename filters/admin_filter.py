41cee8f2ca9e03fd12606d625e1b693ade43e608
Nemoyden <robert.kharazov@gmail.com>
2025-06-08 13:23:27 +0300

    Update admin_filter.py

--- filters/admin_filter.py
+++ filters/admin_filter.py
@@ -1,1 +1,8 @@
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
 
+ADMINS = [123456789]  # Замените на свой user_id
+
+class AdminFilter(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in ADMINS
