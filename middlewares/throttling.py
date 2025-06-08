f6c1805d78b103d23f3a10d071b3751611eedc8c
Nemoyden <robert.kharazov@gmail.com>
2025-06-08 13:24:38 +0300

    Update throttling.py

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -1,1 +1,15 @@
+from aiogram.dispatcher.middlewares.base import BaseMiddleware
+from aiogram.types import Message
+import asyncio
 
+class ThrottlingMiddleware(BaseMiddleware):
+    def __init__(self, rate_limit=1.0):
+        self.rate_limit = rate_limit
+        self.users = {}
+
+    async def __call__(self, handler, event, data):
+        user_id = event.from_user.id
+        if user_id in self.users:
+            await asyncio.sleep(self.rate_limit)
+        self.users[user_id] = True
+        return await handler(event, data)
