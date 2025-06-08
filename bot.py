797942eda97fa74b776341aa9530909d57976301
Nemoyden <robert.kharazov@gmail.com>
2025-06-08 13:09:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,24 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from config.settings import BOT_TOKEN
+from routers import commands
+from routers.handlers import specific_handlers
+from middlewares.throttling import ThrottlingMiddleware
+from utils.logger import logger
 
+async def main():
+  
+    bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+    dp = Dispatcher(storage=MemoryStorage())
+
+    dp.message.middleware(ThrottlingMiddleware())
+
+    dp.include_router(commands.router)
+    dp.include_router(specific_handlers.router)
+
+    logger.info("Бот запущен")
+    await dp.start_polling(bot)
+
+if name == "__main__":
+    asyncio.run(main())
